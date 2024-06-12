1329ffc001595b5b92827e02aacf82596f1f3026
Spoopage <88647165+Spoopage@users.noreply.github.com>
2024-06-12 12:51:40 +0700

    Update connection.php

--- Proyek ADSI/includes/connection.php
+++ Proyek ADSI/includes/connection.php
@@ -14,8 +14,16 @@ $pengiriman = new Products($conn);
 $session_login = isset($_SESSION['login']) ? $_SESSION['login'] : '';
 
 if (isset($session_login)) {
-    $fetch_admin = "SELECT * FROM admins WHERE id = ?";
-    $fetch_admin = $conn->prepare($fetch_admin);
-    $fetch_admin->execute([$session_login]);
-    $fetch_admin = $fetch_admin->fetch();
+    // $fetch_admin = "SELECT * FROM admins WHERE id = ?";
+    // $fetch_admin = $conn->prepare($fetch_admin);
+    // $fetch_admin->execute([$session_login]);
+    // $fetch_admin = $fetch_admin->fetch();
+
+    $fetch_pelanggan = "SELECT * FROM pelanggan WHERE id_pelanggan = ?";
+    $fetch_pelanggan = $conn->prepare($fetch_pelanggan);
+    $fetch_pelanggan->execute([$session_login]);
+    $fetch_pelanggan = $fetch_pelanggan->fetch();
+} else {
+    // $fetch_admin = null;
+    $fetch_pelanggan = null;
 }
